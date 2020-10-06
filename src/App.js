9db4994a1df6a832b866eaf103f445f84d57172a
dnsdudrla97 <dnsdudrla97@naver.com>
2020-10-06 16:07:36 +0900

    dynamic component in mapsss

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React from 'react';
 
-function Com1({name}) {
+function Com1({ name, img }) {
   // console.log(props)
-return <h1>{name}</h1>;
+  return (
+    <div>
+      <h2>{name}</h2>
+      <img src={img}/>
+
+    </div>
+    
+
+
+  )
 }
+
+const dynamicComponent = [
+  {
+    name: "component1",
+    image: "https://media.giphy.com/media/12HZukMBlutpoQ/giphy.gif"
+  },
+  {
+    name: "component1_1",
+    image: "https://media.giphy.com/media/11quO2C07Sh2oM/giphy.gif"
+  },
+  {
+    name: "component1_2",
+    image: "https://media.giphy.com/media/5Vy3WpDbXXMze/giphy.gif"
+  },
+  {
+    name: "component1_3",
+    image: "https://media.giphy.com/media/Ifn0uK1HpgaWykIWVW/giphy.gif"
+  },
+]
+
 
 function App() {
   return (
     <div className="App">
-      <h1>code</h1>
-      <Com1 name="component1"/>
-      <Com1 name="component1_1"/>
-      <Com1 name="component1_2"/>
-      <Com1 name="component1_3"/>
+      <h1>Dynamic Component</h1>
+      {
+        dynamicComponent.map(comp =>
+          <Com1
+            name={comp.name}
+            img={comp.image}
+          />
+        )
+      }
     </div>
 
   );
