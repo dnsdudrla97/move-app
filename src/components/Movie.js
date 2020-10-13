9dc91c3c482b0be250aff7c8fced53472d1c0eea
dnsdudrla97 <dnsdudrla97@naver.com>
2020-10-13 13:45:34 +0900

    test

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -23,11 +23,14 @@ function Movie({ id, year, title, summary, poster, genres }) {
           <h3 className="movie__title">{title}</h3>
           <h5 className="movie__year">{year}</h5>
           <ul className="movie__genres">
-            {genres.map((genre, index) => (
-              <li key={index} className="genres__genre">
-                {genre}
-              </li>
-            ))}
+            {
+              genres.map((genre, index) =>
+                (
+                  <li key={index} className="genres__genre">
+                    {genre}
+                  </li>
+                ))
+            }
           </ul>
           <p className="movie__summary">{summary.slice(0, 180)}...</p>
         </div>
